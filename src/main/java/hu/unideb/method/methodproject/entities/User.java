c9d887666445b13fe2f89ad64d91e3523a461af2
veromihaly <veromihaly18@gmail.com>
2023-10-25 12:34:07 +0200

    Edited User class

--- src/main/java/hu/unideb/method/methodproject/entities/User.java
+++ src/main/java/hu/unideb/method/methodproject/entities/User.java
@@ -1,6 +1,31 @@
 package hu.unideb.method.methodproject.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
 public class User {
+    @Id
     String username;
+    @Column(name = "PASSWORD")
     String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
