f22786584c6090274cf8cb3eeba6376cc6f5078f
PappDávid <PappDavid32@gmail.com>
2023-10-25 10:32:52 +0200

    initial commit

---
+++ src/main/java/hu/unideb/method/methodproject/MethodprojectApplication.java
@@ -0,0 +1,13 @@
+package hu.unideb.method.methodproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MethodprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MethodprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/unideb/method/methodproject/MethodprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.unideb.method.methodproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MethodprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
